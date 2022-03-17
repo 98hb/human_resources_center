99400d8d99da1d0969c12c972033ca42efc33015
‘98hb <w17339377595@163.com>
2022-03-17 10:10:17 +0800

    项目模块初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // 项目标题
+  title: 'Human Resources Center',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 头部固定
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 侧边栏logo
+  sidebarLogo: false
+}
