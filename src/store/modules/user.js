baba76d77d600c5659a5f5fd9b62cb6028088e68
‘98hb <w17339377595@163.com>
2022-03-17 18:45:48 +0800

    在vuex中对token进行管理

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,3 +1,21 @@
+// 从缓存中读取token,将token写入缓存,删除token
+import { getToken, setToken, removeToken } from '@/utils/auth'
+// 状态
+const state = {
+  token: getToken // 设置token 为共享状态,初始化vuex的时候，就先从缓存中读取
+}
+const mutations = {
+  setToken(state, token) {
+    state.token = token // 将数据设置给vuex
+    // 同步给缓存
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null // 将vuex的数据置空
+    removeToken() // 同步到缓存
+  }
+}
+const actions = {}
 export default {
   namespaced: true,
   state: {},
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,18 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'vue_admin_template_token'
+const TokenKey = 'hrsaas-ihrm-token' // 设置一个独一无二的key
 
 export function getToken() {
+  // 从缓存中读取token
   return Cookies.get(TokenKey)
 }
 
 export function setToken(token) {
+  // 将token写入缓存
   return Cookies.set(TokenKey, token)
 }
 
 export function removeToken() {
+  // 删除token
   return Cookies.remove(TokenKey)
 }
