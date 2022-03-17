8d9004d44814cfea3c563249b289e2d4d970a1bd
‘98hb <w17339377595@163.com>
2022-03-17 20:47:45 +0800

    在vuex中对token进行管理

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -4,7 +4,8 @@ import { getToken, setToken, removeToken } from '@/utils/auth'
 const state = {
   token: getToken // 设置token 为共享状态,初始化vuex的时候，就先从缓存中读取
 }
-const mutations = {
+const mutations = { // 同步函数
+  // 两个参数 第一个是状态，第二个是载荷 state,pryload
   setToken(state, token) {
     state.token = token // 将数据设置给vuex
     // 同步给缓存
