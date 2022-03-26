7631ff8c9aeada3371aca04b7cff861e0322be9e
‘98hb <w17339377595@163.com>
2022-03-26 20:51:53 +0800

    树形组件认识

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -29,6 +29,9 @@
             </el-row>
           </el-col>
         </el-row>
+        <!-- el-tree -->
+        <el-tree :data="list" :props="defaultProps" :default-expand-all="true" />
+        <!-- /el-tree -->
       </el-card>
       <!-- /头部 -->
       <!-- /组织架构内容 -->
@@ -38,7 +41,49 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      list: [{
+        label: '一级 1',
+        children: [{
+          label: '二级 1-1',
+          children: [{
+            label: '三级 1-1-1'
+          }]
+        }]
+      }, {
+        label: '一级 2',
+        children: [{
+          label: '二级 2-1',
+          children: [{
+            label: '三级 2-1-1'
+          }]
+        }, {
+          label: '二级 2-2',
+          children: [{
+            label: '三级 2-2-1'
+          }]
+        }]
+      }, {
+        label: '一级 3',
+        children: [{
+          label: '二级 3-1',
+          children: [{
+            label: '三级 3-1-1'
+          }]
+        }, {
+          label: '二级 3-2',
+          children: [{
+            label: '三级 3-2-1'
+          }]
+        }]
+      }],
+      defaultProps: {
+        label: 'label', // 表示从这个属性显示内容
+        children: 'children' // 从这个属性去找子节点
+      }
+    }
+  }
 }
 </script>
 
