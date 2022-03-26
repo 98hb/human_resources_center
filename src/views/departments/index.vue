6aeb5f13dd7bb9e35f12ecef83689f3511f8eb5d
‘98hb <w17339377595@163.com>
2022-03-27 01:09:10 +0800

    实现树形的静态组织结构

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -5,14 +5,14 @@
       <!-- 头部 -->
       <el-card class="tree-card">
         <!-- 放置结构内容 -->
-        <el-row type="flex" justify="space-between">
+        <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
           <!-- 左侧内容 -->
           <el-col>
             <span>江苏传智播客教育科技股份有限公司</span>
           </el-col>
           <!-- 右侧内容 -->
           <el-col :span="4">
-            <el-row type="flex" justify="end" align="middle" style="height: 40px">
+            <el-row type="flex" justify="end">
               <el-col>负责人</el-col>
               <!-- 下拉菜单 -->
               <el-col>
@@ -30,7 +30,37 @@
           </el-col>
         </el-row>
         <!-- el-tree -->
-        <el-tree :data="list" :props="defaultProps" :default-expand-all="true" />
+        <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
+          <!-- 传入插槽内容且循环遍历节点 -->
+          <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据 data 每个节点的数据对象 -->
+          <!-- data, node 要 data -->
+          <el-row slot-scope="{data}" type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
+            <!-- 左侧内容 -->
+            <el-col>
+              <span>{{ data.name }}</span>
+            </el-col>
+            <!-- 右侧内容 -->
+            <el-col :span="4">
+              <el-row type="flex" justify="end">
+                <el-col>{{ data.manager }}</el-col>
+                <!-- 下拉菜单 -->
+                <el-col>
+                  <el-dropdown>
+                    <span>操作
+                      <i class="el-icon-arrow-down" />
+                    </span>
+                    <!-- 下拉选项 -->
+                    <el-dropdown-menu slot="dropdown">
+                      <el-dropdown-item>添加子部门</el-dropdown-item>
+                      <el-dropdown-item>编辑部门</el-dropdown-item>
+                      <el-dropdown-item>删除部门</el-dropdown-item>
+                    </el-dropdown-menu>
+                  </el-dropdown>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </el-tree>
         <!-- /el-tree -->
       </el-card>
       <!-- /头部 -->
@@ -43,43 +73,10 @@
 export default {
   data() {
     return {
-      list: [{
-        label: '一级 1',
-        children: [{
-          label: '二级 1-1',
-          children: [{
-            label: '三级 1-1-1'
-          }]
-        }]
-      }, {
-        label: '一级 2',
-        children: [{
-          label: '二级 2-1',
-          children: [{
-            label: '三级 2-1-1'
-          }]
-        }, {
-          label: '二级 2-2',
-          children: [{
-            label: '三级 2-2-1'
-          }]
-        }]
-      }, {
-        label: '一级 3',
-        children: [{
-          label: '二级 3-1',
-          children: [{
-            label: '三级 3-1-1'
-          }]
-        }, {
-          label: '二级 3-2',
-          children: [{
-            label: '三级 3-2-1'
-          }]
-        }]
-      }],
+      departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
+        { name: '行政部', manager: '刘备' }, { name: '人事部', manager: '孙权' }],
       defaultProps: {
-        label: 'label', // 表示从这个属性显示内容
+        label: 'name', // 表示从这个属性显示内容
         children: 'children' // 从这个属性去找子节点
       }
     }
