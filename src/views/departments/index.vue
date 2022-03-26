9aa89dcbd20429d79e4602a5ea3b4ee3c89d7994
‘98hb <w17339377595@163.com>
2022-03-26 16:04:00 +0800

    实现组织架构的头部内容

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,9 +1,37 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <!-- 组织架构内容 -->
+      <!-- 头部 -->
+      <el-card class="tree-card">
+        <!-- 放置结构内容 -->
+        <el-row type="flex" justify="space-between">
+          <!-- 左侧内容 -->
+          <el-col>
+            <span>江苏传智播客教育科技股份有限公司</span>
+          </el-col>
+          <!-- 右侧内容 -->
+          <el-col :span="4">
+            <el-row type="flex" justify="end" align="middle" style="height: 40px">
+              <el-col>负责人</el-col>
+              <!-- 下拉菜单 -->
+              <el-col>
+                <el-dropdown>
+                  <span>操作
+                    <i class="el-icon-arrow-down" />
+                  </span>
+                  <!-- 下拉选项 -->
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-card>
+      <!-- /头部 -->
+      <!-- /组织架构内容 -->
     </div>
   </div>
 </template>
@@ -14,6 +42,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.tree-card {
+  padding: 30px  140px;
+  font-size:14px;
+}
 </style>
