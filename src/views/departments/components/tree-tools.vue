eef6d825527533f29599f27d361e23634fa3b1ae
‘98hb <w17339377595@163.com>
2022-03-27 15:27:36 +0800

    封装单独的树操作栏组件

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,82 @@
+<!--  -->
+<template>
+  <el-row type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
+    <!-- 左侧内容 -->
+    <el-col>
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <!-- 右侧内容 -->
+    <el-col :span="4">
+      <el-row type="flex" justify="end">
+        <el-col>{{ treeNode.manager }}</el-col>
+        <!-- 下拉菜单 -->
+        <el-col>
+          <el-dropdown>
+            <span>操作
+              <i class="el-icon-arrow-down" />
+            </span>
+            <!-- 下拉选项 -->
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item>编辑部门</el-dropdown-item>
+              <el-dropdown-item>删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+// 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+// 例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+// import引入的组件需要注入到对象中才能使用
+  name: '',
+  components: {},
+  // props可以用数组来接收数据 也可以用对象来接收
+  // props: {   props属性: {  配置选项 }  }
+  props: {
+    // 定义传入的属性
+    treeNode: {
+      required: true,
+      type: Object
+    }
+  },
+  data() {
+    // 这里存放数据
+    return {
+
+    }
+  },
+  // 监听属性 类似于data概念
+  computed: {},
+  // 监控data中的数据变化
+  watch: {},
+  // 生命周期 - 创建完成（可以访问当前this实例）
+  created() {
+
+  },
+  // 生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {
+
+  },
+  beforeCreate() {}, // 生命周期 - 创建之前
+  beforeMount() {}, // 生命周期 - 挂载之前
+  beforeUpdate() {}, // 生命周期 - 更新之前
+  updated() {}, // 生命周期 - 更新之后
+  beforeDestroy() {}, // 生命周期 - 销毁之前
+  destroyed() {}, // 生命周期 - 销毁完成
+  activated() {},
+  // 方法集合
+  methods: {
+
+  } // 如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+<style lang='less' scoped>
+//@import url(); 引入公共css类
+
+</style>
