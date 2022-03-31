5c5259dae4f0ad90295d1b2c2cc56962721e8e68
‘98hb <w17339377595@163.com>
2022-04-01 07:32:09 +0800

    读取角色列表数据

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -9,11 +9,12 @@
             <el-row style="height: 60px">
               <el-button type="primary" icon="el-icon-plus" size="small">新增角色</el-button>
             </el-row>
-            <el-table border="">
-              <el-table-column label="序号" width="120" />
-              <el-table-column label="名称" width="240" />
-              <el-table-column label="描述" />
-              <el-table-column label="操作">
+            <!-- 给表格绑定数据 -->
+            <el-table border="" :data="list">
+              <el-table-column align="center" type="index" label="序号" width="120" />
+              <el-table-column align="center" prop="name" label="名称" width="240" />
+              <el-table-column align="center" prop="description" label="描述" />
+              <el-table-column align="center" label="操作">
                 <el-button size="small" type="success">分配权限</el-button>
                 <el-button size="small" type="primary">编辑</el-button>
                 <el-button size="small" type="danger">删除</el-button>
@@ -21,10 +22,20 @@
             </el-table>
             <!-- 放置分页组件 -->
             <el-row type="flex" justify="center" align="middle" style="height:60px">
-              <el-pagination layout="prev, pager, next" />
+              <!--:current-page="page.page" 当前页码
+              :page-size="page.pagesize" 每页条数
+              :total="page.total" 总数
+              -->
+              <el-pagination
+                :current-page="page.page"
+                :page-size="page.pagesize"
+                :total="page.total"
+                layout="prev, pager, next"
+                @current-change="changePage"
+              />
             </el-row>
           </el-tab-pane>
-          <el-tab-pane label="公司信息" name="fourth">
+          <el-tab-pane label="公司信息">
             <el-alert title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改" :closable="false" type="info" :show-icon="true" />
             <!-- 右侧内容 -->
             <el-form label-width="120px" style="margin-top:50px">
@@ -52,8 +63,34 @@
 </template>
 
 <script>
+import { getRoleList } from '@/api/setting'
 export default {
-
+  data() {
+    return {
+      list: [], // 承接数组
+      page: {
+        // 放置页码及相关数据
+        page: 1,
+        pagesize: 10,
+        total: 0 // 记录总数
+      }
+    }
+  },
+  created() {
+    this.getRoleList() // 获取角色列表
+  },
+  methods: {
+    async getRoleList() {
+      const { total, rows } = await getRoleList(this.page)
+      this.page.total = total
+      this.list = rows
+    },
+    changePage(newPage) {
+      // newPage 是当前点击的页码
+      this.page.page = newPage // 将当前页码赋值给当前的最新页码
+      this.getRoleList()
+    }
+  }
 }
 </script>
 
