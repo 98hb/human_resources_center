f37211272eb5fb93086b8026492c31ceda20920f
‘98hb <w17339377595@163.com>
2022-04-03 16:04:06 +0800

    8 员工导入组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 // 负责所有全局自定义组件的注册
 // 一次编写，永远运行
 import PageTools from './PageTools'
+import UploadExcel from './UploadExcel'
 export default {
   install(Vue) {
     // 组件的注册
     Vue.component('PageTools', PageTools) // 'PageTools'组件名称， PageTools 组件对象
+    Vue.component('UploadExcel', UploadExcel) // 'UploadExcel'组件名称， UploadExcel 组件对象
   }
 }
 // Vue.use()
