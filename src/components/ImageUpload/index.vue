9b005f1b17ea91e7b68392c20fcec2e9d0ee9a92
‘98hb <w17339377595@163.com>
2022-04-05 14:27:19 +0800

    点击图片进行预览

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -1,8 +1,15 @@
 <!--  -->
 <template>
-  <el-upload list-type="picture-card">
-    <i class="el-icon-plus" />
-  </el-upload>
+  <div>
+    <!-- 给 action 一个 # 就不会报错 -->
+    <!-- file-list 是上传的文件列表，可以绑定到上传组件上，让上传组件来显示 -->
+    <el-upload list-type="picture-card" :limit="1" action="#" :on-preview="preview" :file-list="fileList">
+      <i class="el-icon-plus" />
+    </el-upload>
+    <el-dialog :visible.sync="showDialog" title="图片预览">
+      <img :src="imgUrl" alt="" style="width:100%">
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -19,7 +26,9 @@ export default {
   data() {
     // 这里存放数据
     return {
-
+      fileList: [{ url: 'https://img1.baidu.com/it/u=1406801134,3159244927&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=313' }],
+      showDialog: false,
+      imgUrl: ''
     }
   },
   // 监听属性 类似于data概念
@@ -43,7 +52,12 @@ export default {
   activated() {},
   // 方法集合
   methods: {
-
+    // 点击预览事件
+    preview(file) {
+    //   console.log(file.url)
+      this.imgUrl = file.url
+      this.showDialog = true
+    }
   } // 如果页面有keep-alive缓存功能，这个函数会触发
 }
 </script>
