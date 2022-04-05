e8b29e2feaf3daf93c0eec096e26bb4fbc0cf1c5
‘98hb <w17339377595@163.com>
2022-04-05 16:44:45 +0800

    删除图片和添加图片

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -3,18 +3,21 @@
   <div>
     <!-- 给 action 一个 # 就不会报错 -->
     <!-- file-list 是上传的文件列表，可以绑定到上传组件上，让上传组件来显示 -->
+    <!-- upload 组件显示的是 file-list -->
     <el-upload
       list-type="picture-card"
-      :limit="1"
       action="#"
+      :limit="1"
       :on-preview="preview"
+      :on-remove="handleRemove"
+      :on-change="changeFile"
       :file-list="fileList"
       :class="{ disabled : fileComputed }"
     >
       <i class="el-icon-plus" />
     </el-upload>
     <el-dialog :visible.sync="showDialog" title="图片预览">
-      <img :src="imgUrl" alt="" style="width:100%">
+      <img :src="imgUrl" style="width:100%" alt="">
     </el-dialog>
   </div>
 </template>
@@ -33,14 +36,14 @@ export default {
   data() {
     // 这里存放数据
     return {
-      fileList: [{ url: 'https://img1.baidu.com/it/u=1406801134,3159244927&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=313' }],
+      fileList: [{ url: 'https://t7.baidu.com/it/u=3676218341,3686214618&fm=193&f=GIF' }],
       showDialog: false,
       imgUrl: ''
     }
   },
   // 监听属性 类似于data概念
   computed: {
-    // 如果它为 true 表示就不该显示 + 号
+    // 设定一个计算属性 判断是否已经上传完了一张
     fileComputed() {
       return this.fileList.length === 1
     }
@@ -69,12 +72,30 @@ export default {
     //   console.log(file.url)
       this.imgUrl = file.url
       this.showDialog = true
+    },
+    // file 是要删除的文件
+    // fileList 是删过之后的文件
+    handleRemove(file, fileList) {
+    //   console.log(file)
+    //   console.log(fileList)
+    //   console.log(this.fileList)
+      this.fileList = this.fileList.filter(item => item.uid !== file.uid) // 将当前的删除文件排除在外
+    //   this.fileList = fileList
+    },
+    // 不能用 push 这个钩子会执行多次
+    changeFile(file, fileList) {
+    //   console.log(file)
+    //   console.log(fileList)
+      this.fileList = fileList.map(item => item)
+      // 这里为何暂时不成功讷？ 因为现在还没有上传，所以第二次进来的数据一定是个空的。
+      // 如果完成上传动作了，第一次进入和第二次进入的 fileList 的长度应该都是1，应该都有数据
+      // 上传成功 => 数据才能进来 => 腾讯云 cos
     }
   } // 如果页面有keep-alive缓存功能，这个函数会触发
 }
 </script>
-<style scoped>
+<style>
   .disabled .el-upload--picture-card {
-  display: none
+  display: none;
 }
 </style>
