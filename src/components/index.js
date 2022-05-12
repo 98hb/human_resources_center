995c43e94b5f0be87efb8c751aa61b3a42386de6
‘98hb <w17339377595@163.com>
2022-05-12 15:05:38 +0800

    利用vue-print-nb进行打印

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,14 @@
 import PageTools from './PageTools'
 import UploadExcel from './UploadExcel'
 import ImageUpload from './ImageUpload'
+import Print from 'vue-print-nb'
 export default {
   install(Vue) {
     // 组件的注册
     Vue.component('PageTools', PageTools) // 'PageTools'组件名称， PageTools 组件对象
     Vue.component('UploadExcel', UploadExcel) // 'UploadExcel'组件名称， UploadExcel 组件对象
     Vue.component('ImageUpload', ImageUpload) // 'ImageUpload'组件名称， ImageUpload组件对象
+    Vue.use(Print) // 注册打印组件
   }
 }
 // Vue.use()
